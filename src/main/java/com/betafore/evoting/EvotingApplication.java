4a0b9d5aaf9d19302cbf4c023625f7318420e7da
Tanveer Ahmed <md.tanveeraj@gmail.com>
2025-08-07 17:54:59 +0600

    refactored main app

--- src/main/java/com/betafore/evoting/EvotingApplication.java
+++ src/main/java/com/betafore/evoting/EvotingApplication.java
@@ -6,24 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EvotingApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(EvotingApplication.class, args);
     }
-
-//    @Bean
-//    @Transactional
-//    public CommandLineRunner commandLineRunner(OrganizerService service) {
-//        fileStorageServiceImp.initStorage();
-//        return args -> {
-//            OrganizerDto superAdmin = OrganizerDto.builder()
-//                .phoneNumber(phoneNumber)
-//                .email(email)
-//                .password(password)
-//                .enabled(enabled)
-//                .build();
-//            service.createSuperAdmin(superAdmin, superAdminRole);
-//        };
-//    }
 
 }
